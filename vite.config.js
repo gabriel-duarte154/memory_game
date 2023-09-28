36f91b05938a1a173bc7b43fde272c08224dc845
Gabriel <gabrieljunior-github@proton.me>
2023-09-28 09:48:35 -0300

    Generate dist folder

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,23 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        assetFileNames: (assetInfo) => {
+          let extType = assetInfo.name.split('.').at(1);
+          if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(extType)) {
+            extType = 'img';
+          }
+          if (/ttf/i.test(extType)) {
+            extType = 'font';
+          }
+
+          return `assets/${extType}/[name]-[hash][extname]`;
+        },
+        chunkFileNames: 'assets/js/[name]-[hash].js',
+        entryFileNames: 'assets/js/[name]-[hash].js',
+      },
+    },
+  },
 });
